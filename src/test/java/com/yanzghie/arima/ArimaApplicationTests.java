f5935d8cd8d7fd558ab75848b3581e15b912e670
yangzhie <arnavmalasi2212@gmail.com>
2025-11-09 19:10:34 +1100

    Create Spring boilerplate code.

---
+++ src/test/java/com/yanzghie/arima/ArimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanzghie.arima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
